0bb82fc591b2e57dfe06080f580c7035cce840d1
s-ourav <sourav.mozu@gmail.com>
2023-12-21 00:28:55 +0530

    second

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void Main() {
         B obj =new B();
         obj.meth();
-        A objj=new B();
-        objj.meth();
+//        A objj=new B();
+        obj.meth();
     }
 
 
