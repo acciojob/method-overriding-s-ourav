4298d2054a9c88b51ffe67666df48c4ecb9d24ad
s-ourav <sourav.mozu@gmail.com>
2023-12-21 17:17:24 +0530

    don

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,12 +9,14 @@ public class Main {
     }
 
     public static class B extends A {
+        @Override
         String meth (){
             return "Method is overridden in Extendend class B";
         }
     }
 
     public static void main(String args[]) {
+
         B obj =new B();
         System.out.println(obj.meth());
     }
